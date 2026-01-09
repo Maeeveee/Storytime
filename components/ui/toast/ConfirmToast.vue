fd89ff7817cc2962ea973a45a136ff5628d7195a
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2026-01-09 17:45:54 +0800

    feat: update VSCode settings to disable auto-compilation and add ConfirmToast component for user notifications

---
+++ components/ui/toast/ConfirmToast.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps<{ text: string }>();
+</script>
+<template>
+    <div class="toast">
+        <div class="toast__content">
+            <Icon class="toast__content__icon--primary" name="lucide:circle-check-big" />
+            <h5 class="toast__content__text">{{ text }}</h5>
+        </div>
+        <Icon class="toast__content__icon--secondary" name="formkit:close" />
+    </div>
+</template>
+<style scoped lang="scss">
+.toast {
+    width: 600px;
+    height: 70px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &__content {
+        display: flex;
+        align-items: center;
+        gap: 16px;
+
+        &__icon {
+            width: 30px;
+            height: 30px;
+
+            &--primary {
+                color: var(--color-primary);
+            }
+
+            &--secondary {
+                margin-left: 104.5px;
+                color: var(--color-text-secondary)
+            }
+        }
+
+        &__text {
+            font-weight: 700;
+            font-size: 20px;
+            line-height: 26px;
+        }
+    }
+}
+</style>
