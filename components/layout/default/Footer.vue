4e85328671b77905856ed16993152bb1a3acc2a6
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2026-01-15 09:08:02 +0800

    style: fix typo in font-vw mixin and refactor Footer component for improved responsiveness

--- components/layout/default/Footer.vue
+++ components/layout/default/Footer.vue
@@ -16,72 +16,61 @@ const copyright: string = `© ${currentyear} Timedoor Indonesia. All rights rese
 </template>
 
 <style scoped lang="scss">
-@media screen and (max-width: 768px) {
-    .footer {
-        &__content {
-            margin-top: 50px;
-            padding:10px;
-            display: flex;
+.footer {
+    &__content {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        border-top: 1px solid var(--color-text-secondary);
+
+        @include desktop {
+            margin-top: vw(200);
+            padding: vw(25) vw(110) vw(35);
+        }
+
+        @include mobile {
             flex-direction: column;
-            align-items: center;
-            justify-content: space-between;
-            border-top: #4b4b4b 1px solid;
-        }
-
-        &__text {
-            margin-top: 30px;
-            margin-bottom: 10px;
-            font-weight: 500;
-            font-size: 15px;
-            line-height: 20px;
-            color: #4b4b4b;
-        }
-
-        &__icons {
-            font-size: 30px;
-            display: flex;
-            gap: 20px;
-
-            &__icon {
-                &:hover {
-                    color: var(--color-primary);
-                }
-            }
+            margin-top: vw-mobile(59);
+            padding: vw-mobile(10);
         }
     }
-}
 
-@media screen and (min-width: 768px) {
-    .footer {
-        &__content {
-            margin-top: 200px;
-            padding: 0 110px;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            border-top: #4b4b4b 1px solid;
+    &__text {
+        font-weight: 500;
+        color: var(--color-text-secondary);
+
+        @include desktop {
+            font-size: vw(20);
+            line-height: vw(26);
         }
 
-        &__text {
-            margin-top: 30px;
-            margin-bottom: 40px;
-            font-weight: 500;
-            font-size: 20px;
-            line-height: 26px;
-            color: #4b4b4b;
+        @include mobile {
+            margin-top: vw-mobile(30);
+            margin-bottom: vw-mobile(20);
+            font-size: vw-mobile(15);
+            line-height: vw-mobile(26);
+        }
+    }
+
+    &__icons {
+        display: flex;
+
+        @include desktop {
+            font-size: vw(40);
+            gap: vw(20);
         }
 
-        &__icons {
-            font-size: 40px;
-            display: flex;
-            gap: 20px;
+        @include mobile {
+            font-size: vw-mobile(30);
+            gap: vw-mobile(20);
+        }
 
-            &__icon {
-                &:hover {
-                    color: var(--color-primary);
-                }
+        &__icon {
+            &:hover {
+                color: var(--color-primary);
             }
         }
+
     }
 }
 </style>
